3ddc4a6098582e7cf5f994d7e8674e66c32928a0
ADyBaH <Brandon4@mail.ru>
2023-01-16 13:07:48 +0300

    feat: add solution for morse-decorer

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const answer = [];
+    let chank = '';
+    let count = 0;
+    expr = expr.replace("**********", "  ")
+    for (let i = 0; i < expr.length; i += 2) {
+        count += 1
+        if(`${expr[i]}${expr[i + 1]}` === '10') chank += '.';
+        if(`${expr[i]}${expr[i + 1]}` === '11') chank += '-';
+        if(`${expr[i]}${expr[i + 1]}` === '  ') {
+        answer.push(" ");
+        chank = '';
+        count = 0;
+        };
+        if(count == 5) {
+        answer.push(chank);
+        chank = '';
+        count = 0;
+        }
+    }
+    return answer.map(e => (MORSE_TABLE[e])? MORSE_TABLE[e] : ' ').join('');
 }
 
 module.exports = {
